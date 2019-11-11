90219e5044dfc7f9c82614296ef997d15477690f
derinozon <derinozon@gmail.com>
2019-11-11 11:07:40 +0300

    post-test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ function FadeHello (scrollPos) {
 	var x = document.getElementById("h-hello");
 	let a = (lim - scrollPos) / lim;
 	if (a < 0) return;
-	x.style.opacity = a;
+	//x.style.opacity = a;
+	let info = document.getElementById("info");
+	info.style.opacity = a;
+}
+
+function HandleContact () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             alert(this.responseText);
+         }
+    };
+    xhttp.open("POST", "http://paper.rf.gd/api/", true);
+    xhttp.setRequestHeader("Content-type", "text/plain");
+    xhttp.send("Your JSON Data Here");
 }
 
 window.addEventListener("scroll", (event) => {
@@ -24,3 +38,42 @@ for (let i = 0; i < btns.length; i++) {
 
 var btn = btns[0];
 btn.classList.add("active-editor-btn");
+
+
+
+/*
+async function postData(url = '', data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+	  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+	  mode: 'cors', // no-cors, *cors, same-origin
+	  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+	  credentials: 'same-origin', // include, *same-origin, omit
+	  headers: {
+		'Content-Type': 'application/json'
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+	  },
+	  redirect: 'follow', // manual, *follow, error
+	  referrer: 'no-referrer', // no-referrer, *client
+	  body: JSON.stringify(data) // body data type must match "Content-Type" header
+	});
+	return await response.json(); // parses JSON response into native JavaScript objects
+  }
+
+
+
+
+function ftch () {
+
+	
+
+	try {
+		const data = await postData('http://paper.rf.gd/api/');
+		console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
+	  } catch (error) {
+		console.error(error);
+	  }
+	  
+	  
+}
+*/
