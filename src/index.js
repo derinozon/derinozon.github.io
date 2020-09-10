c638ce5f0c7969bc4278531b69a5876d902ce021
derinozon <derinozon@gmail.com>
2020-09-10 13:50:22 +0300

    index update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ function FadeHello (scrollPos) {
 	var x = document.getElementById("h-hello");
 	let a = (lim - scrollPos) / lim;
 	if (a < 0) return;
-	//x.style.opacity = a;
 	let info = document.getElementById("info");
 	info.style.opacity = a;
 }
@@ -38,42 +37,3 @@ for (let i = 0; i < btns.length; i++) {
 
 var btn = btns[0];
 btn.classList.add("active-editor-btn");
-
-
-
-/*
-async function postData(url = '', data = {}) {
-	// Default options are marked with *
-	const response = await fetch(url, {
-	  method: 'POST', // *GET, POST, PUT, DELETE, etc.
-	  mode: 'cors', // no-cors, *cors, same-origin
-	  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-	  credentials: 'same-origin', // include, *same-origin, omit
-	  headers: {
-		'Content-Type': 'application/json'
-		// 'Content-Type': 'application/x-www-form-urlencoded',
-	  },
-	  redirect: 'follow', // manual, *follow, error
-	  referrer: 'no-referrer', // no-referrer, *client
-	  body: JSON.stringify(data) // body data type must match "Content-Type" header
-	});
-	return await response.json(); // parses JSON response into native JavaScript objects
-  }
-
-
-
-
-function ftch () {
-
-	
-
-	try {
-		const data = await postData('http://paper.rf.gd/api/');
-		console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
-	  } catch (error) {
-		console.error(error);
-	  }
-	  
-	  
-}
-*/
