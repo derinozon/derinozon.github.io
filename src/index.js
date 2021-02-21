07b4b81b5f797924a3c152f512db25d6267b4617
derinozon <derinozon@gmail.com>
2021-02-21 15:15:04 +0300

    Video test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 
 function FadeHello (scrollPos) {
 	let lim = document.getElementById("top-img").offsetHeight / 2;
-	var x = document.getElementById("h-hello");
+	
 	let a = (lim - scrollPos) / lim;
 	if (a < 0) return;
 	let info = document.getElementById("info");
 	info.style.opacity = a;
-}
-
-function HandleContact () {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
-             alert(this.responseText);
-         }
-    };
-    xhttp.open("POST", "http://paper.rf.gd/api/", true);
-    xhttp.setRequestHeader("Content-type", "text/plain");
-    xhttp.send("Your JSON Data Here");
 }
 
 window.addEventListener("scroll", (event) => {
