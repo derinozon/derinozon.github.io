37a7cbdb5eff98964715d9fba79c20dc045818e6
derinozon <derinozon@gmail.com>
2021-02-24 17:43:54 +0300

    low test

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
+const hello = document.getElementById("h-hello");
 
 function FadeHello (scrollPos) {
-	let lim = document.getElementById("top-img").offsetHeight / 2;
+	let lim = hello.offsetHeight / 2;
 	
 	let a = (lim - scrollPos) / lim;
 	if (a < 0) return;
-	let info = document.getElementById("info");
-	info.style.opacity = a;
+	
+	hello.style.opacity = a;
 }
 
 window.addEventListener("scroll", (event) => {
 	FadeHello(this.scrollY);
 });
-
-nav = document.getElementById("editor-nav");
-btns = nav.getElementsByTagName("button");
-for (let i = 0; i < btns.length; i++) {
-	btns[i].addEventListener("click",() => {
-		for (let a = 0; a < btns.length; a++) {
-			btns[a].classList.remove("active-editor-btn");
-		}
-		btns[i].classList.add("active-editor-btn");
-	});
-}
-
-var btn = btns[0];
-btn.classList.add("active-editor-btn");
