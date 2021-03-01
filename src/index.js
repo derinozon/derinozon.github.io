dd293c6f5f9b42bbb14c03db87b791fb396eb8f3
derinozon <derinozon@gmail.com>
2021-03-01 15:12:48 +0300

    skills tab

--- src/index.js
+++ src/index.js
@@ -9,6 +9,58 @@ function FadeHello (scrollPos) {
 	hello.style.opacity = a;
 }
 
-window.addEventListener("scroll", (event) => {
+window.addEventListener("scroll", () => {
 	FadeHello(this.scrollY);
 });
+
+// Terminal FX //
+
+const con = document.getElementById('console');
+const target = document.getElementById('text');
+
+function consoleText(words, colors) {
+	if (colors === undefined) colors = ['#fff'];
+
+	var letterCount = 1;
+	var x = 1;
+	var waiting = false;
+
+	target.setAttribute('style', 'color:' + colors[0])
+	window.setInterval(() => {
+		var waitTime = 800;
+
+		if (letterCount === 0 && waiting === false) {
+			waiting = true;
+	    	target.innerHTML = words[0].substring(0, letterCount);	
+
+			window.setTimeout(() => {
+				colors.push(colors.shift());
+				words.push(words.shift());
+
+				x = 1;
+				target.setAttribute('style', 'color:' + colors[0])
+				letterCount += x;
+				waiting = false;
+			}, waitTime)
+	  	}
+		else if (letterCount === words[0].length + 1 && waiting === false) {
+			waiting = true;
+			window.setTimeout(() => {
+				x = -1;
+				letterCount += x;
+				waiting = false;
+			}, waitTime)
+		}
+		else if (waiting === false) {
+			target.innerHTML = words[0].substring(0, letterCount)
+			letterCount += x;
+		}
+	}, 100)
+
+  	// Blink //
+	window.setInterval(
+		() => con.className = (con.className === '') ? 'hidden' : ''
+	, 400)
+}
+
+consoleText(['Python', 'Node JS', 'C++', 'Java FX'], ['lightblue','yellow','magenta', 'brown']);
