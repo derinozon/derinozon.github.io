4d61925b9a0498107514002506c29cde4f56ad02
@machina <derinozon@gmail.com>
2023-01-27 16:00:22 +0100

    First 2023 update

--- src/index.js
+++ src/index.js
@@ -16,96 +16,79 @@ window.addEventListener("scroll", () => {
 	FadeVideo();
 });
 
-const userAction = async () => {
-	console.log("getting data");
-	let l1 = 'http://dev.revolando.com/api/mail.php?msg=hello';
-	let l2 = 'https://dero-mailapi.herokuapp.com/index.php?msg=hello';
-	const response = await fetch(l2);
-	
-	alert(response.ok);
-}
-
-async function SendMailServer () {
-	let content = document.getElementById("mailtext").value;
-	if (content === '') return;
-
-	let sender = document.getElementById("mailsender").value;
-	if (sender === '') sender = "example@gmail.com";
-
-	let l2 = 'https://dero-mailapi.herokuapp.com/index.php?msg=' + content + "&sender=" + sender;
-	const response = await fetch(l2);
-
-	if (!response.ok) {
-		alert("You can't send two mails at the same day");
-	}
-	else {
-		alert("Your mail has been sent successfully");
-	}
-}
-
 let proj_index = 0;
-
-function SwitchProject (i) {
-	const pimg = document.getElementById("project-image");
-	const ph = document.getElementById("project-header");
-	const pp = document.getElementById("project-description");
-
-	var list = JSON.parse( `[{
+const pimg = document.getElementById("project-image");
+const ph = document.getElementById("project-header");
+const pp = document.getElementById("project-description");
+const list = JSON.parse(`[
+	{
+		"img": "res/img/tumor.png",
+		"header": "Brain Tumor Detector",
+		"link": "_",
+		"info": "University project developed with Aw Thura that detects if an MRI scan contains a tumor or not using Random Forest Classifier"
+	},
+	{
 		"img": "res/img/catch_me.png",
 		"header": "CatchMe!",
 		"link": "https://derinozon.itch.io/catchme",
 		"info": "Made a game together with Aydın Özön in a week for Nokia 3310 Jam 3. Developed a 2D Game Engine from scratch in C++ for this game jam resulting in a lightweight and fast game that is less than 1MB"
-	  },
-	  {
+	},
+	{
 		"img": "res/img/vloop.png",
 		"header": "Virtual Loop",
 		"link": "https://frauma.itch.io/virtualloop",
 		"info": "Simulated a physics bending non euclidian world using tricks such as portals in Frauma Entertainments submission of 72 hour Mağara Jam 3."
-	  },
-	  {
+	},
+	{
 		"img": "res/img/cyp.png",
 		"link": "_",
 		"header": "CYP Auto",
 		"info": "Worked as an app developer for Revolando and b4bynd. Developed and built the Android/IOS port of the web based automobile sales platform CYP Auto in a week"
-	  },
-	  {
+	},
+	{
 		"img": "res/img/kopia.png",
 		"link": "https://derinozon.itch.io/kopia",
 		"header": "Kopia",
 		"info": "Frauma Entertainments submission on ATOM Game Jam 2 scoring #1 in popularity in 407 entries. Coded an exam cheating simulator with an AI teacher that tracks your behaviour using optimised methods in 72 hours."
-	  },
-	  {
+	},
+	{
 		"img": "res/img/housein.png",
 		"header": "ArchViz Demo 2019",
 		"link": "https://www.youtube.com/watch?v=NxP5cp1gnN8",
 		"info": "Did the level design and programming on this ArchViz demo in highschool"
-	  },
-	  {
+	},
+	{
 		"img": "res/img/this2.png",
 		"header": "This Website",
 		"link": "_",
 		"info": "Coded this lightweight staticly served website from scratch with modern HTML CSS and JS without using any frameworks or external resources."
-	  }
-	]` );
+	}
+]`);
+
+function SwitchProject (i) {
+	pimg.src = "res/static.gif";
 	proj_index += i;
+	
+	window.setInterval( () => {
+		if (proj_index < 0) {
+			proj_index = list.length-1;
+		}
+		if (proj_index >= list.length) {
+			proj_index = 0;
+		}
+		
+		pimg.src = list[proj_index].img;
+		if (list[proj_index].link == '_') {
+			pimg.parentElement.style.pointerEvents = "none";
+		}
+		else {
+			pimg.parentElement.style.pointerEvents = "auto";
+			pimg.parentElement.href = list[proj_index].link;
+		}
+		ph.innerHTML = list[proj_index].header;
+		pp.innerHTML = list[proj_index].info;
 
-	if (proj_index < 0) {
-		proj_index = list.length-1;
-	}
-	if (proj_index >= list.length) {
-		proj_index = 0;
-	}
-
-	pimg.src = list[proj_index].img;
-	if (list[proj_index].link == '_') {
-		pimg.parentElement.style.pointerEvents = "none";
-	}
-	else {
-		pimg.parentElement.style.pointerEvents = "auto";
-		pimg.parentElement.href = list[proj_index].link;
-	}
-	ph.innerHTML = list[proj_index].header;
-	pp.innerHTML = list[proj_index].info;
+	}, 1000);
 }
 
 function SendMail () {
